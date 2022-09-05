fbc8f8326e57c219333b19a5842bf2ef62a022cc
MWasile <maaciej.wasilewski@gmail.com>
2022-09-05 16:16:06 +0200

    celery work test

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ScrapTask(models.Model):
+    scrap_id = models.CharField(max_length=100)
+
+
+class ScheduleTest(models.Model):
+    sth = models.IntegerField()
--- api/views.py
+++ api/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework import views
 from rest_framework.response import Response
 
+from api.tasks import scrap_test
+
 
 class Test(views.APIView):
     def get(self, request):
+        scrap_test.apply_async()
         return Response({'Status': 'OK'})
