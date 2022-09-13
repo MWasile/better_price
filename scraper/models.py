468dec3b5db72ebe6a6553a1b12e70696a3288bc
MWasile <maaciej.wasilewski@gmail.com>
2022-09-13 21:19:12 +0200

    remove past models

--- scraper/models.py
+++ scraper/models.py
@@ -3,21 +3,6 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 
 
-class TaskWorkInfo(models.Model):
-    task_type = models.CharField(max_length=10)
-    search_key = models.CharField(max_length=200)
-    email = models.EmailField(null=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
-    status = models.BooleanField(default=True)
-
-
-class ScrapTask(models.Model):
-    owner_task = models.ForeignKey('TaskWorkInfo', on_delete=models.CASCADE, related_name='scrap_tasks')
-    data = models.JSONField()
-
-
-# ---------------2.0 CONCEPT----------------
-
 class BaseTaskInfo(models.Model):
     task_type = models.CharField(max_length=10)
     user_ebook = models.CharField(max_length=200)
@@ -40,6 +25,6 @@ class EmailTaskInfo(BaseTaskInfo):
 class ScrapResult(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
-    data = models.JSONField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
+    data = models.JSONField()
