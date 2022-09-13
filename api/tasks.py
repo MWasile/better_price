43a898cc100328e0da40c9f7fffaa221cb9f3366
MWasile <maaciej.wasilewski@gmail.com>
2022-09-13 21:21:55 +0200

    base tasks concept

--- api/tasks.py
+++ api/tasks.py
@@ -1,16 +1,37 @@
 import random
 
 from celery import shared_task
-from api.models import ScrapTask, ScheduleTest
+import scraper.new_scraper as sc
+from scraper.models import EmailTaskInfo
 
 
 @shared_task(queue='scrap_queue')
-def scrap_test():
-    x = ScrapTask.objects.create(scrap_id='XDDD')
-    x.save()
+def fast_scrap_task(task):
+    tasks_to_run = sc.TaskFactory.create_task_istance(
+        task['type'],
+        task['user_ebook'],
+        task['scrap_model']
+    )
+
+    for task in tasks_to_run:
+        task.scrap()
 
 
 @shared_task(queue='email_queue')
 def email():
-    x = ScheduleTest.objects.create(sth=1)
-    x.save()
+    print('zyje')
+    all_tasks = EmailTaskInfo.objects.filter(status=True)
+
+    for task in all_tasks:
+        ts = sc.TaskManager('email', user_ebook=task.user_ebook,
+                            email_case_model=task.id, user_price_alert=task.price)
+        ts.run_celery_task()
+
+
+@shared_task(queue='email_queue')
+def email_send():
+    all_tasks = EmailTaskInfo.objects.filter(status=None)
+
+    for task in all_tasks:
+        #TODO: send email, data -> task.results.all
+        pass
