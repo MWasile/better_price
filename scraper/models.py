f9e37cdc22ed78a16502ef8e6d9b3c4b85b2fa3d
MWasile <maaciej.wasilewski@gmail.com>
2022-09-13 11:31:57 +0200

    models concept with contentype

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 
@@ -13,3 +14,32 @@ class TaskWorkInfo(models.Model):
 class ScrapTask(models.Model):
     owner_task = models.ForeignKey('TaskWorkInfo', on_delete=models.CASCADE, related_name='scrap_tasks')
     data = models.JSONField()
+
+
+# ---------------2.0 CONCEPT----------------
+
+class BaseTaskInfo(models.Model):
+    task_type = models.CharField(max_length=10)
+    user_ebook = models.CharField(max_length=200)
+    results = GenericRelation('ScrapResult')
+
+    class Meta:
+        abstract = True
+
+
+class FastTaskInfo(BaseTaskInfo):
+    pass
+
+
+class EmailTaskInfo(BaseTaskInfo):
+    email = models.EmailField(null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    status = models.BooleanField(default=True)
+
+
+class ScrapResult(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+    data = models.JSONField()
+
