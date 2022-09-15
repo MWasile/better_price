28080b4269ed729ab2adff3d94b82f421af8ca41
MWasile <maaciej.wasilewski@gmail.com>
2022-09-15 17:08:21 +0200

    concept

---
+++ scraper/async_scraper.py
@@ -0,0 +1,13 @@
+# TASK -> ONLY DATA
+
+# MANAGER ->
+# pinned model
+# prepare json celery info
+# passing data to worker
+
+
+# SCRAPENGINE -> run with json info as worker
+# create taks classes
+# prepare task
+# async requesti
+# async bs4
