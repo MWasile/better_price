d80a76768960f6e76512f1cce38fc5b9a82a9da3
MWasile <maaciej.wasilewski@gmail.com>
2022-09-15 20:19:18 +0200

    2 bookstores scrap test, aio beat slow requests

--- scraper/async_scraper.py
+++ scraper/async_scraper.py
@@ -6,43 +6,56 @@ import time
 import bs4
 import requests
 
+woblink = {
+    'url': 'https://woblink.com/katalog/ebooki?szukasz=korepetytor',
+    'first_qs': 'ul.catalog-items.lista',
+    'second_qs': 'div [data-item-layout="tiles"]',
+    'title_qs': 'a.catalog-tile__title span',
+}
+empik = {
+    'url': 'https://www.empik.com/audiobooki-i-ebooki,35,s?q=korepetytor&qtype=basicForm',
+    'first_qs': 'div.container.search-results.js-search-results',
+    'second_qs': 'div.search-list-item',
+    'title_qs': '.ta-product-title',
+}
+
+t = [woblink, empik]
+
 
 class Engine:
-    async def scrap(self, session, url):
-        async with session.get(url) as response:
+    async def scrap(self, session, task):
+        async with session.get(task['url']) as response:
             html = await response.text()
 
-        prettify = await self.prettify_response(html)
+        prettify = await self.prettify_response(html, task)
         return prettify
 
     @staticmethod
-    async def prettify_response(data_to_prettify):
+    async def prettify_response(data_to_prettify, task):
         # async def parser_html(data):
         #     tag_soup = bs4.BeautifulSoup(data, 'lxml')
         #     return tag_soup
         #
         # t_soup = await parser_html(data_to_prettify)
         t_soup = bs4.BeautifulSoup(data_to_prettify, 'lxml')
-        ebook_main_container = t_soup.select_one('ul.catalog-items.lista')
-        all_ebooks = ebook_main_container.select('div [data-item-layout="tiles"]')
+        ebook_main_container = t_soup.select_one(task['first_qs'])
+        all_ebooks = ebook_main_container.select(task['second_qs'])
 
         for i in all_ebooks:
-            title = i.select_one('a.catalog-tile__title span')
+            title = i.select_one(task['title_qs'])
+            print('aio: ', title.getText())
             return title
 
     async def setup_task(self, tasks):
         tasks_to_run = []
         async with aiohttp.ClientSession() as session:
-            for url in tasks:
-                tasks_to_run.append(self.scrap(session, url))
+            for task in tasks:
+                tasks_to_run.append(self.scrap(session, task))
 
             web_result = await asyncio.gather(*tasks_to_run)
-            print(web_result)
 
 
-t = ['https://woblink.com/katalog/ebooki?szukasz=korepetytor']
 
-random.shuffle(t)
 
 start = time.time()
 test = Engine()
@@ -51,6 +64,14 @@ print(f'aiohttp: {time.time() - start}')
 
 
 class ScrapEnginge_2:
+    def __init__(self, tasks):
+        self.tasks = tasks
+
+    def run_run(self):
+
+        for t in self.tasks:
+            x = self.scrap_request(t['url'])
+            self.prettify_response(x, t)
 
     def scrap_request(self, url):
         try:
@@ -63,21 +84,20 @@ class ScrapEnginge_2:
 
         return False
 
-    def prettify_response(self, raw_data):
+    def prettify_response(self, raw_data, task):
         tag_soup = bs4.BeautifulSoup(raw_data, 'lxml')
 
-        ebook_main_container = tag_soup.select_one('ul.catalog-items.lista')
-        all_ebooks = ebook_main_container.select('div [data-item-layout="tiles"]')
+        ebook_main_container = tag_soup.select_one(task['first_qs'])
+        all_ebooks = ebook_main_container.select(task['second_qs'])
 
         for i in all_ebooks:
-            title = i.select_one('a.catalog-tile__title span')
+            title = i.select_one(task['title_qs'])
+            print('Request: ', title.getText())
             return title
         return False
 
 
 start2 = time.time()
-test_2 = ScrapEnginge_2()
-raw_data = test_2.scrap_request(t[0])
-x = test_2.prettify_response(raw_data)
-print(x)
+test_2 = ScrapEnginge_2(t)
+test_2 = test_2.run_run()
 print(f'requests: {time.time() - start2}')
