69a2f595d40d726afdcf5b0afe7709464c4a9f0d
MWasile <maaciej.wasilewski@gmail.com>
2022-09-17 18:42:55 +0200

    refresh taskmanager

--- scraper/async_scraper.py
+++ scraper/async_scraper.py
@@ -1,11 +1,16 @@
 import asyncio
 import dataclasses
+import sys
 from dataclasses import astuple, asdict
 from dataclasses import dataclass
 from decimal import Decimal
 
 import aiohttp
 import bs4
+from django.core.exceptions import ValidationError
+
+from config import settings
+from scraper.models import FastTaskInfo, EmailTaskInfo
 
 
 class ScrapEngine:
@@ -68,3 +73,68 @@ class Task:
 
         # TODO: DB AUTOSAVE
         self._data = data_from_bookstores
+
+
+class TaskManager:
+    def __init__(self, user_input, email_case=False, email_price=False, email_model_id=None):
+        self.user_input = user_input
+        self.email = email_case, email_price, email_model_id
+        self.model_id = self._pin_model()
+        self.tasks = self._pin_task()
+
+    def _pin_model(self):
+        # Task Email already has model created before, so escape.
+        if not self.email[0]:
+            return self.email[2]
+
+        new_core_model = FastTaskInfo(
+            task_type='fast',
+            user_ebook=self.user_ebook
+        )
+
+        try:
+            new_core_model.full_clean()
+            new_core_model.save()
+        except ValidationError:
+            return None
+
+        return new_core_model.id
+
+    def _pin_task(self):
+        try:
+            bookstore_settings = settings.SCRAPER_BOOKSTORES
+        except AttributeError:
+            raise ImportError("SCRAPER_BOOKSTORES must be specified in django settings.")
+
+        if not bookstore_settings:
+            raise ValueError(f'Bookstores class name must be specified in SCRAPER_BOOKSTORES')
+
+        mix_inheritances = tuple(
+            (Task, getattr(sys.modules[__name__], import_name) for import_name in bookstore_settings)
+        )
+
+        bases = [type('ReadyTask', mixin_inheritance) for mixin_inheritance in mix_inheritances]
+
+        tasks = [asdict(mix_class(self.model_id, self.user_input, email=self.email[0], email_price=self.email[1]))
+                 for mix_class in bases]
+
+        return tasks
+
+    @classmethod
+    def create_email_task(cls, user_input, user_email, user_price):
+        new_email_task = EmailTaskInfo(
+            task_type='email',
+            user_ebook=user_input,
+            email=user_email,
+            price=user_price
+        )
+
+        try:
+            new_email_task.full_clean()
+            new_email_task.save()
+        except ValidationError:
+            return False
+        return new_email_task
+
+    def run(self):
+        pass
