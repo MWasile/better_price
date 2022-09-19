eca44ac520ffbd7c0d865b8851349b1347ca3d5a
MWasile <maaciej.wasilewski@gmail.com>
2022-09-19 13:59:17 +0200

    asgi setup, add routing websocket

--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,25 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+import example_channels.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                example_channels.routing.websocket_urlpatterns
+            )
+        )
+    )
+})
