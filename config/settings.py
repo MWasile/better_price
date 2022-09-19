f0d486bfe11d0d0a9dd4b776201c84b920f1cfea
MWasile <maaciej.wasilewski@gmail.com>
2022-09-19 12:44:05 +0200

    email config

--- config/settings.py
+++ config/settings.py
@@ -174,3 +174,12 @@ SCRAPER_BOOKSTORES = [
     'Woblink',
     'Empik'
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
