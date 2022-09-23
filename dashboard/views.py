0bc9796bc7dd28b56e907557358d55d1db983030
MWasile <maaciej.wasilewski@gmail.com>
2022-09-23 18:20:52 +0200

    dashboard view, all user searched inputs with pagination

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,20 @@
 from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+from scraper import models
 
 
-class DashboardHome(TemplateView):
+class DashboardHome(ListView):
+    model = models.FastTaskInfo
+    context_object_name = 'user_results'
     template_name = 'dashboard/dashboard_home.html'
+    paginate_by = 2
 
     def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect(reverse_lazy('users:login'))
         return super().get(request, *args, **kwargs)
 
+    def get_queryset(self):
+        return models.FastTaskInfo.objects.filter(search_for=self.request.user.id)
