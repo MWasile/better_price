eff088302968c915a51859c4d0051671e7566e63
MWasile <maaciej.wasilewski@gmail.com>
2022-09-23 19:11:49 +0200

    detail view for scrap result in dashboard history

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('home', views.DashboardHome.as_view(), name='home')
+    path('home', views.DashboardHistory.as_view(), name='home'),
+    path('detail/<int:pk>', views.ResultsDetail.as_view(), name='detail'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView, DetailView, ListView
 from scraper import models
 
 
-class DashboardHome(ListView):
+class DashboardHistory(ListView):
     model = models.FastTaskInfo
     context_object_name = 'user_results'
     template_name = 'dashboard/dashboard_home.html'
@@ -18,3 +18,13 @@ class DashboardHome(ListView):
 
     def get_queryset(self):
         return models.FastTaskInfo.objects.filter(search_for=self.request.user.id)
+
+
+class ResultsDetail(ListView):
+    model = models.ScrapEbookResult
+    template_name = 'dashboard/details.html'
+    context_object_name = 'details'
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        return models.ScrapEbookResult.objects.filter(object_id=pk)
