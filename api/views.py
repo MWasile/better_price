7e8b10c924ae0ca66c0b2aeb9701589093fb1222
MWasile <maaciej.wasilewski@gmail.com>
2022-09-24 09:10:57 +0200

    create email task via drf

--- api/views.py
+++ api/views.py
@@ -1,9 +1,27 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
-from rest_framework import views
+from rest_framework import views, status
 from rest_framework.response import Response
 from scraper import tasks
+from scraper import models
 
 
-class Test(views.APIView):
-    def get(self, request):
-        return Response({'Status': 'OK'})
+class CreateEmailTask(views.APIView):
+    def post(self, request, title, price):
+
+        if request.user.is_authenticated:
+            email = models.EmailTaskInfo(
+                user_input_search=title,
+                user_price_alert=price,
+                user_email=request.user.email,
+                search_for=self.request.user
+            )
+
+            try:
+                email.full_clean()
+                email.save()
+            except ValidationError:
+                return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(status=status.HTTP_200_OK)
+
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
